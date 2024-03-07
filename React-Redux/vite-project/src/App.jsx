a9f0a7c28d3ff903ea336abe92616499fe2690b0
Samaresh <samareshshil165@gmail.com>
2024-03-07 16:39:04 +0530

    redux commit

---
+++ React-Redux/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Button } from "@mui/material";
+import "./App.css"
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="main">
+      <h1>{count}</h1>
+      <Button onClick={() => setCount(count + 1)} variant="contained" color="secondary" size="small">Click me</Button>
+      <Button onClick={() => setCount(count - 1)} variant="contained" color="secondary" size="small">Click me</Button>
+    </div>
+  );
+};
+
+export default App;
